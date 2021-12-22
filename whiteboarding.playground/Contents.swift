203e4a3d4954fa46d9ec7277b5804a20e3109251
Jenny Morales <50502203+jennyrm@users.noreply.github.com>
2021-12-22 14:19:28 -0800

    Integer Reversal

--- whiteboarding.playground/Contents.swift
+++ whiteboarding.playground/Contents.swift
@@ -1,58 +1,88 @@
-///String Reversal
-func reverseString(_ str: String) -> String {
-    return String(str.reversed())
-}
+/////String Reversal
+//func reverseString(_ str: String) -> String {
+//    return String(str.reversed())
+//}
+//
+//func reverseString1(_ str: String) -> String {
+//    var reversedStr = [Character]()
+//
+//    str.forEach { reversedStr.insert($0, at: 0) }
+//
+//    //character sequence initializer of string
+//    return String(reversedStr)
+//}
+//
+//func reverseString2(_ str: String) -> String {
+//    return str.map { $0 }.reduce("") { String($1) + String($0) }
+//}
+//
+//reverseString2("Jenny")
+//
+/////Palindrome
+//func isPalindrome(str: String) -> Bool {
+//    return str == String(str.reversed())
+//}
+//
+//func isPalindrome1(str: String) -> Bool {
+//    let length = str.count / 2
+//    for i in 0..<length {
+//        print(i)
+//        let start = str.index(str.startIndex, offsetBy: i)
+//        let end = str.index(str.endIndex, offsetBy: (-(i + 1)))
+//
+//        if str[start] != str[end] {
+//            print("Start:", str[start])
+//            print("End:", str[end])
+//            return false
+//        }
+//    }
+//    return true
+//}
+//
+//func isPalindrome2(str: String) -> Bool {
+//    guard str.count >= 2 else { return true }
+//
+//    let end = str.index(str.endIndex, offsetBy: -1)
+//
+//    if str[str.startIndex] == str[end] {
+//        let start = str.index(str.startIndex, offsetBy: 1)
+//
+//        print(str[start..<end])
+//
+//        return isPalindrome2(str: String(str[start..<end]))
+//    }
+//
+//    return false
+//}
+//
+//print(isPalindrome2(str: "tacocat"))
 
-func reverseString1(_ str: String) -> String {
-    var reversedStr = [Character]()
+///Integer Reversal
+func reverseInt(_ int: Int) -> Int {
     
-    str.forEach { reversedStr.insert($0, at: 0) }
-    
-    //character sequence initializer of string
-    return String(reversedStr)
-}
-
-func reverseString2(_ str: String) -> String {
-    return str.map { $0 }.reduce("") { String($1) + String($0) }
-}
-
-reverseString2("Jenny")
-
-///Palindrome
-func isPalindrome(str: String) -> Bool {
-    return str == String(str.reversed())
-}
-
-func isPalindrome1(str: String) -> Bool {
-    let length = str.count / 2
-    for i in 0..<length {
-        print(i)
-        let start = str.index(str.startIndex, offsetBy: i)
-        let end = str.index(str.endIndex, offsetBy: (-(i + 1)))
-        
-        if str[start] != str[end] {
-            print("Start:", str[start])
-            print("End:", str[end])
-            return false
-        }
+    let reversed = abs(int)
+    if int < 0 {
+        return (-Int(String(String(reversed).reversed()))!)
+    } else {
+        return Int(String(String(reversed).reversed()))!
     }
-    return true
+    
 }
 
-func isPalindrome2(str: String) -> Bool {
-    guard str.count >= 2 else { return true }
-    
-    let end = str.index(str.endIndex, offsetBy: -1)
-    
-    if str[str.startIndex] == str[end] {
-        let start = str.index(str.startIndex, offsetBy: 1)
-
-        print(str[start..<end])
-       
-        return isPalindrome2(str: String(str[start..<end]))
+func reverse(_ x: Int) -> Int {
+    var remain = abs(x)
+    var reverseNum = 0
+    while remain != 0 {
+        reverseNum = (reverseNum * 10) + (remain % 10)
+        print("reverseNum:", reverseNum)
+        remain = remain/10
+        print("remain:", remain)
     }
-    
-    return false
+    if reverseNum > Int32.max {
+        return 0
+    }
+    return x > 0 ? reverseNum : -reverseNum
 }
 
-print(isPalindrome2(str: "tacocat"))
+reverse(98)
+
